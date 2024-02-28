defc5ec840aab98a9d3d18142d42f61c5fdf344c
stud <stud@W04N-P317D12>
2024-02-28 20:01:32 +0100

    Add project files.

---
+++ lab0_jszulc/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace lab0_jszulc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz f1= new FizzBuzz(20);
+            f1.Display();
+        }
+    }
+}
